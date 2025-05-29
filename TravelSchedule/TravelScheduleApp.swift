ee7784199bdd220d4b37cf0a06f2b8fa7fae3a3d
teenagelove <dani4upa586@gmail.com>
2025-05-29 19:43:09 +0900

    chore(project setup): initial Xcode project creation and configuration

---
+++ TravelSchedule/TravelScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelScheduleApp.swift
+//  TravelSchedule
+//
+//  Created by Danil Kazakov on 29.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct TravelScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
