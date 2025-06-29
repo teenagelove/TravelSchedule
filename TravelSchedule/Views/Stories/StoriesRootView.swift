7143428427020d7faa38a5a7f22742996c46fe12
teenagelove <dani4upa586@gmail.com>
2025-06-29 16:03:19 +0900

    feat: implemente close story on swipe

--- TravelSchedule/Views/Stories/StoriesRootView.swift
+++ TravelSchedule/Views/Stories/StoriesRootView.swift
@@ -8,7 +8,9 @@
 import SwiftUI
 
 struct StoriesRootView: View {
-    let viewModel: TopicsViewModel
+    @Bindable var viewModel: TopicsViewModel
+    @State private var dragOffset: CGSize = .zero
+    @Environment(\.dismiss) private var dismiss
     
     var body: some View {
         ZStack(alignment: .topTrailing) {
@@ -17,6 +19,36 @@ struct StoriesRootView: View {
                 .padding(.top, 57)
                 .padding(.trailing, 12)
         }
+        .offset(y: dragOffset.height)
+        .opacity(1 - abs(dragOffset.height) / 500.0)
+        .gesture(
+            DragGesture()
+                .onChanged({ value in
+                    if dragCondition(value.translation) {
+                        dragOffset = CGSize(width: 0, height: value.translation.height)
+                    }
+                })
+                .onEnded({ value in
+                    if dragCondition(value.translation) && value.translation.height > 150 {
+                        dismiss()
+                    } else {
+                        withAnimation(.spring()) {
+                            dragOffset = .zero
+                        }
+                    }
+                })
+        )
+    }
+}
+
+private extension StoriesRootView {
+    func dragCondition(_ translation: CGSize) -> Bool {
+        let horizontalAmount = abs(translation.width)
+        let verticalAmount = abs(translation.height)
+        let isStrictlyVertical = verticalAmount > horizontalAmount * 3
+        let isDownward = translation.height > 0
+        
+        return isDownward && isStrictlyVertical
     }
 }
 
