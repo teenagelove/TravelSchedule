449d7680f9782ac9368bb5ce66fac98a7abd0cb7
teenagelove <dani4upa586@gmail.com>
2025-07-15 19:12:33 +0900

    refactor: refactor seiwtch/case

--- TravelSchedule/Models/TimeSlot.swift
+++ TravelSchedule/Models/TimeSlot.swift
@@ -27,14 +27,10 @@ extension TimeSlot {
         let hour = calendar.component(.hour, from: date)
         
         switch self {
-        case .morning:
-            return hour >= 6 && hour < 12
-        case .day:
-            return hour >= 12 && hour < 18
-        case .evening:
-            return hour >= 18 && hour < 24
-        case .night:
-            return hour >= 0 && hour < 6
+        case .morning: return hour >= 6 && hour < 12
+        case .day: return hour >= 12 && hour < 18
+        case .evening: return hour >= 18 && hour < 24
+        case .night: return hour >= 0 && hour < 6
         }
     }
 }
